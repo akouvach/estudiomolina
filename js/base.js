39a2c9d21775ce6f9213ac4f35ff5e3b48b1cc69
Andres Kouvach <akouvach@yahoo.com>
2023-08-01 10:44:44 -0300

    buscador

---
+++ js/base.js
@@ -0,0 +1,103 @@
+console.log(articulos);
+console.log(temas);
+
+// Initialize tooltips
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+  return new bootstrap.Tooltip(tooltipTriggerEl)
+})
+
+
+var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
+var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+    return new bootstrap.Popover(popoverTriggerEl)
+})
+
+// document.getElementById("toastbtn").onclick = function() {
+//     var toastElList = [].slice.call(document.querySelectorAll('.toast'))
+//     var toastList = toastElList.map(function(toastEl) {
+//         return new bootstrap.Toast(toastEl)
+//     })
+//     toastList.forEach(toast => toast.show()) 
+// }
+
+function buscar(){
+    let valor = document.getElementById("txt_buscar").value;
+    let destino = document.getElementById("buscar_resultados");
+    let tabla = "";
+    let rdo = [];
+    
+
+    /* Muestro la ventana modal que estaba oculta */
+    let myModal = new bootstrap.Modal(document.getElementById('modal_resultados'));
+    let btnCerrrar = document.getElementById('btn_cerrar');
+
+    btnCerrrar.addEventListener('click', ()=>{
+        myModal.hide();
+    });
+    
+    myModal.show();
+
+    /* voy a buscar lo que encuentre y armo el resultado */
+
+    timeoutID = setTimeout(()=>{
+        /* busco los resultados en articulos */
+        for(let i=0;i<articulos.length;i++){
+            let pos = -1;
+            pos = articulos[i].titulo.toLowerCase().search(valor.toLowerCase());
+            if(pos>=0){
+                /* encontró algo */
+                rdo.push(articulos[i]);
+            } else {
+                pos = articulos[i].tema.toLowerCase().search(valor.toLowerCase());
+                if(pos>=0){
+                    /* encontró algo */
+                    rdo.push(articulos[i]);
+                } else {
+                    pos = articulos[i].contenido.toLowerCase().search(valor.toLowerCase());
+                    if(pos>=0){
+                        /* encontró algo */
+                        rdo.push(articulos[i]);
+                    }        
+                }
+            }
+        }
+
+        if(rdo.length>0){
+            /* Encontro algo.. voy a armar la tabla */
+            tabla = `<table class="table">
+            <thead class="thead-dark">
+              <tr>
+              <th scope="col">Fecha</th>
+              <th scope="col">Tema</th>
+              <th scope="col">Documento</th>
+                <th scope="col"> >> </th>
+              </tr>
+            </thead>
+            <tbody>`;
+
+            for(let index=0;index<rdo.length;index++){
+                tabla = tabla.concat(`
+                <tr>
+                    <td>${rdo[index].fecha}</td>
+                    <td>${rdo[index].tema}</td>
+                    <td>${rdo[index].titulo}-${rdo[index].contenido}</td>
+                    <td><a href="${rdo[index].fuente}" target="detallefuente"> ver </a></td>
+                </tr>
+                `)
+            }
+            tabla = tabla.concat(`
+                </tbody>
+            </table>`);
+            destino.innerHTML = tabla;
+        } else {
+            /* no se encontraron resultados */
+            destino.innerHTML = `<p class="container"> no se encontraron resultados </p>`;
+        }
+      
+    }, 1000 );
+
+}
+
+document.getElementById("form_buscar").addEventListener("submit", buscar, false);
+
---
+++ js/datos.js
@@ -0,0 +1,12 @@
+let temas = ["previsional","sucesiones","despidos","daños"];
+
+let articulos = [
+    {
+        "titulo":"Nuevo sueldo de nuestros abuelos",
+        "tema":"previsional",
+        "contenido":"En este articulo se recupera como debe abonarse los montos correspondientes a los jubilados",
+        "fecha":"31/8/2022",
+        "fuente":"https://www.clarin.com/economia/anses-anuncio-bono-36-000-jubilados-cobrara-paso_0_erXsdtSNlT.html"
+    }
+];
+
